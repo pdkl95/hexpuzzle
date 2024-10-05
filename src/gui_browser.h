aef083405e2fe3617c98b46e5eeb8a1ab530c301
Brent Sanders <git@thoughtnoise.net>
2024-10-05 00:17:17 -0700

    added templates for gui_browser/gui_options

---
+++ src/gui_browser.h
@@ -0,0 +1,30 @@
+/****************************************************************************
+ *                                                                          *
+ * gui_browser.h                                                            *
+ *                                                                          *
+ * This file is part of hexpuzzle.                                          *
+ *                                                                          *
+ * hexpuzzle is free software: you can redistribute it and/or               *
+ * modify it under the terms of the GNU General Public License as published *
+ * by the Free Software Foundation, either version 3 of the License,        *
+ * or (at your option) any later version.                                   *
+ *                                                                          *
+ * hexpuzzle is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General *
+ * Public License for more details.                                         *
+ *                                                                          *
+ * You should have received a copy of the GNU General Public License along  *
+ * with hexpuzzle. If not, see <https://www.gnu.org/licenses/>.             *
+ *                                                                          *
+ ****************************************************************************/
+
+#ifndef GUI_BROWSER_H
+#define GUI_BROWSER_H
+
+struct gui_browser {
+};
+typedef struct gui_browser gui_browser_t;
+
+#endif /*GUI_BROWSER_H*/
+
---
+++ src/gui_options.h
@@ -0,0 +1,30 @@
+/****************************************************************************
+ *                                                                          *
+ * gui_options.h                                                            *
+ *                                                                          *
+ * This file is part of hexpuzzle.                                          *
+ *                                                                          *
+ * hexpuzzle is free software: you can redistribute it and/or               *
+ * modify it under the terms of the GNU General Public License as published *
+ * by the Free Software Foundation, either version 3 of the License,        *
+ * or (at your option) any later version.                                   *
+ *                                                                          *
+ * hexpuzzle is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General *
+ * Public License for more details.                                         *
+ *                                                                          *
+ * You should have received a copy of the GNU General Public License along  *
+ * with hexpuzzle. If not, see <https://www.gnu.org/licenses/>.             *
+ *                                                                          *
+ ****************************************************************************/
+
+#ifndef GUI_OPTIONS_H
+#define GUI_OPTIONS_H
+
+struct gui_options {
+};
+typedef struct gui_options gui_options_t;
+
+#endif /*GUI_OPTIONS_H*/
+
